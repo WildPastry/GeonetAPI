f172be4dff2f4e0f255520e9997c0544b4c7ee41
WildPastry <mykdsn@gmail.com>
2019-02-26 20:10:54 +1300

    Updated files

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 /*jslint browser:true */
 console.log('JS Loaded...');
-var mapKey;
-var map;
-var x, y;
+
+// VARIABLES
 var data1 = [],
     data2 = [],
     data3 = [],
     data4 = [],
     title = []
-var marker = [];
-var contents = [];
-var infowindow = [];
+var marker = [],
+    markersArray = [],
+    contents = [],
+    infowindow = [];
+var latList, lngList, depthList;
+var mapKey, map, x, y;
 
-// MAP KEY
+// EVENT LISTENER
+document.getElementById('formSub').addEventListener('click', getValues);
+
+// LOAD MAP KEY
 // $.ajax({
 //     url: 'js/config.json',
 //     dataType: 'json',
@@ -201,6 +206,37 @@ function initMap() {
             }
         ]
     });
+} //INIT MAP
+
+// CLEAR MARKERS
+function clearMarkers() {
+    for (var n = 0; n < markersArray.length; n++) {
+        markersArray[n].setMap(null);
+    }
+    markersArray = [];
+}
+
+// CLEAR INFO WINDOWS
+// function hideAllInfoWindows(map) {
+//     markersArray.forEach(function(marker) {
+//       marker.infowindow.close(map, marker);
+//    }); 
+//  }
+ 
+// FUNCTION
+function getValues() {
+
+    clearMarkers();
+
+    latList = document.getElementById('formLat').value;
+    lngList = document.getElementById('formLng').value;
+    depthList = document.getElementById('formDepth').value;
+    console.log('LAT Value:');
+    console.log(latList);
+    console.log('LNG Value:');
+    console.log(lngList);
+    console.log('Depth Value:');
+    console.log(depthList);
 
     // INFO WINDOWS
     $.ajax({
@@ -215,47 +251,57 @@ function initMap() {
                 var y = getData.features[i].geometry.coordinates[1];
                 var x = getData.features[i].geometry.coordinates[0];
 
-                if ((y < (-41.2840)) && (x < 174.50)) {
+                // if ((y < (-41.2840)) && (x < 174.50)) { //SOUTH ISLAND
+                // if ((y > (-41.2840)) && (x > 174.50)) { //NORTH ISLAND
 
-                    title[i] = getData.features[i].properties.locality;
-                    data1[i] = getData.features[i].properties.time;
-                    data2[i] = getData.features[i].properties.depth;
-                    data3[i] = getData.features[i].properties.magnitude;
-                    data4[i] = getData.features[i].properties.mmi;
+                title[i] = getData.features[i].properties.locality;
+                data1[i] = getData.features[i].properties.time;
+                data2[i] = getData.features[i].properties.depth;
+                data3[i] = getData.features[i].properties.magnitude;
+                data4[i] = getData.features[i].properties.mmi;
 
-                    if (data2[i] > depthList[]) {
+                if (data2[i] > depthList) {
+                    if (y < latList) { //SOUTH ISLAND
+                        if (x < lngList) { //SOUTH ISLAND
+                            // if (y > latList) { //NORTH ISLAND
+                            //     if (x > lngList) { //NORTH ISLAND
 
-                        marker[i] = new google.maps.Marker({
-                            position: {
-                                lat: y,
-                                lng: x
-                            },
-                            map: map,
-                            title: title[i]
-                        });
+                            marker[i] = new google.maps.Marker({
+                                position: {
+                                    lat: y,
+                                    lng: x
+                                },
+                                map: map,
+                                title: title[i]
+                            });
 
-                        marker[i].index = i;
+                            markersArray.push(marker[i]);
+                            marker[i].index = i;
 
-                        contents[i] =
-                            '<div>' +
-                            '<h5 class="margin">' + title[i] + '</h5>' +
-                            '<p>Time: ' + data1[i] + '</p>' +
-                            '<p>Depth: ' + data2[i] + '</p>' +
-                            '<p>Magnitude: ' + data3[i] + '</p>' +
-                            '<p>MMI: ' + data4[i] + '</p>' +
-                            '</div>';
+                            contents[i] =
+                                '<div>' +
+                                '<h5 class="margin">' + title[i] + '</h5>' +
+                                '<p>Time: ' + data1[i] + '</p>' +
+                                '<p>Depth: ' + data2[i] + '</p>' +
+                                '<p>Magnitude: ' + data3[i] + '</p>' +
+                                '<p>MMI: ' + data4[i] + '</p>' +
+                                '</div>';
 
-                        infowindow[i] = new google.maps.InfoWindow({
-                            content: contents[i]
-                        });
+                            infowindow[i] = new google.maps.InfoWindow({
+                                content: contents[i]
+                            });
 
-                        google.maps.event.addListener(marker[i], 'click', function () {
-                            console.log(this.index);
-                            infowindow[this.index].open(map, marker[this.index]);
-                            map.panTo(marker[this.index].getPosition());
-                        });
-                    } //IF DEPTH
-                } //IF COORDINATES
+                            google.maps.event.addListener(marker[i], 'click', function () {
+                                // hideAllInfoWindows(map);
+                                // this.infowindow.open(map, this);
+                                console.log(this.index);
+                                infowindow[this.index].open(map, marker[this.index]);
+                                map.panTo(marker[this.index].getPosition());
+                            });
+                        } //IF LNG
+                    } //IF LAT
+                } //IF DEPTH
+                // } //IF COORDINATES
             } //FOR
 
         }, //SUCSESS
@@ -264,184 +310,35 @@ function initMap() {
             console.log('Error getting data...')
         }
     }) //AJAX
-} //INIT MAP
+} //FUNCTION
 
-// FORM
-document.getElementById('formSub').addEventListener('click', getLAT);
-
-var latList
-var lngList
-var depthList
-
-// FUNCTIONS
-function getLAT(e) {
-    e.preventDefault();
-    latValue = document.getElementById("formLat").value;
-    // latList.splice(0, 1);
-    // latList.push(latValue);
-    getLNG();
-    console.log('LAT Value');
-    console.log(latList);
-}
-
-function getLNG() {
-    lngValue = document.getElementById("formLng").value;
-    // lngList.splice(0, 1);
-    // lngList.push(lngValue);
-    getDEPTH();
-    console.log('LNG Value');
-    console.log(lngList);
-}
-
-function getDEPTH() {
-    depthValue = document.getElementById("formDepth").value;
-    // depthList.splice(0, 1);
-    // depthList.push(depthValue);
-    console.log('DEPTH');
-    console.log(depthList);
-}
-
-// WORKING
-// $.ajax({
-//     url: 'https://api.geonet.org.nz/quake?MMI=3',
-//     dataType: 'json',
-//     type: 'get',
-//     success: function (getData) {
-//         console.log("JSON data loaded...");
-//         console.log(getData.features);
-//         for (var i = 0; i < getData.features.length; i++) {
-//             var y = getData.features[i].geometry.coordinates[1];
-//             var x = getData.features[i].geometry.coordinates[0];
-
-//             var title = getData.features[i].properties.locality;
-//             var data1 = getData.features[i].properties.time;
-//             var data2 = getData.features[i].properties.depth;
-//             var data3 = getData.features[i].properties.magnitude;
-//             var data4 = getData.features[i].properties.mmi;
-
-//             marker[i] = new google.maps.Marker({
-//                 position: {
-//                     lat: y,
-//                     lng: x
-//                 },
-//                 map: map,
-//                 title: title
-//             });
-
-//             marker[i].index = i;
-
-//             contents[i] =
-//                 '<div>' +
-//                 '<h3 class="margin">' + title + '</h3>' +
-//                 '<p>Time: ' + data1 + '</p>' +
-//                 '<p>Depth: ' + data2 + '</p>' +
-//                 '<p>Magnitude: ' + data3 + '</p>' +
-//                 '<p>MMI: ' + data4 + '</p>' +
-//                 '</div>';
-
-//             infowindow[i] = new google.maps.InfoWindow({
-//                 content: contents[i]
-//             });
-
-//             google.maps.event.addListener(marker[i], 'click', function () {
-//                 console.log(this.index);
-//                 infowindow[this.index].open(map, marker[this.index]);
-//                 map.panTo(marker[this.index].getPosition());
-//             });
-//         }
-//     },
-//     error: function (error) {
-//         console.log(error);
-//         console.log('Error getting data...')
+// function setMarkers(map) {
+//     for (var i = 0; i < locations.length; i++) {
+  
+//       var location = locations[i];
+//       var locationInfowindow = new google.maps.InfoWindow({
+//         content: 'Test content',
+//       });
+  
+//       var marker = new google.maps.Marker({
+//         position: {lat: location[1], lng: location[2]},
+//         map: map,
+//         title: location[0],
+//         infowindow: locationInfowindow
+//       });
+  
+//       markers.push(marker);
+  
+//       google.maps.event.addListener(marker, 'click', function() {
+//         hideAllInfoWindows(map);
+//         this.infowindow.open(map, this);
+//       });
+  
 //     }
-// })
-// }
-
-// var map;
-// function initMap() {
-// 	//map
-//         map = new google.maps.Map(document.getElementById('map'), {
-//           center: {lat: -41.2865, lng: 174.7762},
-//           zoom: 6
-//         }); // map
-// }//initMap
-
-// var mylatitude, mylongitude, mydepth;
-
-// function getValues() {
-
-// mylatitude = document.getElementById('latitude').value;
-// mylongitude = document.getElementById('longitude').value;
-// mydepth= document.getElementById('depth').value;
-
-// console.log(mylatitude,mylongitude,mydepth);
-
-// var x,y;// stores latitude and longitude
-// var marker = []; //markers
-// var winCont = []; // content of infowindows
-// var infowindow = []; //infowindows
-// var depth;
-
-// 	$.ajax({
-// 		url:'https://api.geonet.org.nz/quake?MMI=3',
-// 		type:'GET',
-// 		dataType:'json',
-// 		success:function(dataFromJSON){
-// 			console.log(dataFromJSON);
-// 			//loop through all data obects from API
-// 			for(var i=0; i<dataFromJSON.features.length; i++) {
-// 			//longitude
-// 			console.log(dataFromJSON.features[i].geometry.coordinates[0]); //longitude
-// 			y = dataFromJSON.features[i].geometry.coordinates[0]; //longitude
-
-// 			//latitude
-// 			console.log(dataFromJSON.features[i].geometry.coordinates[1]); //latitude
-// 			x = dataFromJSON.features[i].geometry.coordinates[1]; //latitude
-
-// 			if ((x< (-41.2840)) && (y <174.50)){
-
-//                     console.log(dataFromJSON.features[i].properties.depth)
-
-//                      depth= dataFromJSON.features[i].properties.depth;
-
-//                     if (depth > mydepth) {
-//                         console.log(depth);
-
-// 			//markers 
-// 			 marker[i] = new google.maps.Marker({
-// 				position: { lat: x, lng: y },
-// 				map: map,
-// 				title: dataFromJSON.features[i].properties.locality
-// 			});
-
-// 			 //content of infowindows
-// 			 winCont[i] = "time: " + dataFromJSON.features[i].properties.time + "<br>" + "depth: " + dataFromJSON.features[i].properties.depth + "<br>" + "magnitude: " + dataFromJSON.features[i].properties.magnitude;
-
-// 			 // infowindows
-// 			 infowindow[i] = new google.maps.InfoWindow({
-//                     content: winCont[i]
-//                 });
-
-// 			 //marker click eventlistener
-// 			 google.maps.event.addListener(marker[i],'click', (function(marker,content,infowindow){ 
-//                     return function() {
-//                     infowindow.setContent(content);
-//                     infowindow.open(map,marker);
-
-//                     }; //return
-//                 })(marker[i],winCont[i],infowindow[i])); //even.addListener
-
-// 			  } //depth criteria
-// 			} //coordinates criteria
-
-// 		  } //for
-// 		},//success
-
-// 		error:function(error){
-// 			console.log('Error');
-// 			alert("Something wrong");
-// 		} //error
-
-// 	})//ajax
-
-//  } //getValues
+//   }
+  
+//   function hideAllInfoWindows(map) {
+//      markers.forEach(function(marker) {
+//        marker.infowindow.close(map, marker);
+//     }); 
+//   }
